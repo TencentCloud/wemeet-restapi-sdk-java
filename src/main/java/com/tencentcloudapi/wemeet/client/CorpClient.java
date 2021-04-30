78d865993e1a9c7e8715c672073012234923ec64
Wemeet <81743542+wemeet-restapi@users.noreply.github.com>
2021-04-30 10:31:39 +0800

    新增企业仪表盘相关接口

--- src/main/java/com/tencentcloudapi/wemeet/client/CorpClient.java
+++ src/main/java/com/tencentcloudapi/wemeet/client/CorpClient.java
@@ -3,8 +3,7 @@ package com.tencentcloudapi.wemeet.client;
 import com.google.gson.reflect.TypeToken;
 import com.tencentcloudapi.wemeet.common.RequestSender;
 import com.tencentcloudapi.wemeet.common.exception.WemeetSdkException;
-import com.tencentcloudapi.wemeet.models.corp.QueryCorpRecordsRequest;
-import com.tencentcloudapi.wemeet.models.corp.QueryCorpRecordsResponse;
+import com.tencentcloudapi.wemeet.models.corp.*;
 
 /**
  * <p>仪表盘</p>
@@ -17,6 +16,54 @@ public class CorpClient {
 
     public CorpClient(RequestSender sender) {
         this.sender = sender;
+    }
+
+    /**
+     * 获取账户级已结束会议列表
+     */
+    public QueryCorpEndedMeetingsResponse queryEndedMeetings(QueryCorpEndedMeetingsRequest request) throws WemeetSdkException {
+        return sender.request(request, new TypeToken<QueryCorpEndedMeetingsResponse>() {
+        });
+    }
+
+    /**
+     * 获取账户级即将开始的会议列表
+     */
+    public QueryCorpPreStartMeetingsResponse queryPreStartMeetings(QueryCorpPreStartMeetingsRequest request) throws WemeetSdkException {
+        return sender.request(request, new TypeToken<QueryCorpPreStartMeetingsResponse>() {
+        });
+    }
+
+    /**
+     * 获取账户级实时会议列表
+     */
+    public QueryCorpOnlineMeetingsResponse queryOnlineMeetings(QueryCorpOnlineMeetingsRequest request) throws WemeetSdkException {
+        return sender.request(request, new TypeToken<QueryCorpOnlineMeetingsResponse>() {
+        });
+    }
+
+    /**
+     * 获取账户级全量用户列表
+     */
+    public QueryCorpUsersResponse queryUsers(QueryCorpUsersRequest request) throws WemeetSdkException {
+        return sender.request(request, new TypeToken<QueryCorpUsersResponse>() {
+        });
+    }
+
+    /**
+     * 获取账户级角色列表
+     */
+    public QueryCorpRolesResponse queryRoles(QueryCorpRolesRequest request) throws WemeetSdkException {
+        return sender.request(request, new TypeToken<QueryCorpRolesResponse>() {
+        });
+    }
+
+    /**
+     * 获取账户级指定权限的用户列表
+     */
+    public QueryCorpUsersResponse queryUsersByRole(QueryCorpUsersByRoleRequest request) throws WemeetSdkException {
+        return sender.request(request, new TypeToken<QueryCorpUsersResponse>() {
+        });
     }
 
     /**
